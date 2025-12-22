21fc27cebf3e1c615bf024ebd020ece5dd447e6d
Divya Patil <diupatil2002@gmail.com>
2025-12-22 11:31:50 +0530

    Fix user controller and contactController

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import  protect  from "../middleware/authMiddleware.js";
 import multer from "multer";
 import { login, register } from "../controllers/userController.js";
-import { contact } from "../controllers/contactController.js";
+// import { contact } from "../controllers/contactController.js";
 import contact from "../controllers/contactController.js";
 const router = express.Router();
 
